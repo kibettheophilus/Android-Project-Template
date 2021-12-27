6472fc419d874f4d8d4ccd8b1b85c3dc53ed7cb5
kibet <kibettheophilus@gmail.com>
2021-12-27 16:38:27 +0300

    Fix: added missing DAO injection

--- data/src/main/java/dev/kibet/data/di/DataModule.kt
+++ data/src/main/java/dev/kibet/data/di/DataModule.kt
@@ -13,7 +13,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 val dataModule = module {
-    single<MainRepository> { MainRepositoryImpl(get()) }
+    single<MainRepository> { MainRepositoryImpl(get(),get()) }
     single {
         Retrofit.Builder()
             .baseUrl(BASEURL)
